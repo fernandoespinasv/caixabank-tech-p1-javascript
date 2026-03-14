e84e65b1bde70d439d7780e823a4b64758d7e91c
@fernandoespinasv <fernandoespinasv@gmail.com>
2026-03-14 13:12:05 +0100

    Prueba. Fetch a la API y primeros console.log

--- main.js
+++ main.js
@@ -1,8 +1,17 @@
-import { writeFile } from "fs/promises";
-import { render } from "./render.js";
-import { loadUsers } from "./users.js";
+// Antes de crear clases y generar HTML, lo primero es comprobar que
+// podemos conectarnos correctamente a la API y que esta, devuelve datos correctos.
+// Usaremos una API pública del videojuego de Brawl Stars (más detalles en el README).
 
-const users = await loadUsers(100);
-const html = render(users);
-await writeFile('users.html', html);
+const response = await fetch("https://api.brawlapi.com/v1/brawlers");
+
+// Convertimos la respuesta a un objeto JavaScript usando .json()
+
+const data = await response.json();
+
+// Ahora, vamos a comprobar que los datos son correctos (leer README para entender comandos).
+
+
+console.log("Total de brawlers:", data.list.length); // --> 100
+console.log("Primer brawler", data.list[0].name);  // --> GLOWBERT
+
 
