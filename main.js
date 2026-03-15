9387cc147806f5a70cb445a108e9768362c91d0e
@fernandoespinasv <fernandoespinasv@gmail.com>
2026-03-15 13:03:24 +0100

    Segundo HTML

--- main.js
+++ main.js
@@ -1,17 +1,21 @@
-// Vamos a generar ahora nuestro archivo index.html
-
-// Importamos las distintas funciones creadas, además usaremos writeFile de Node.js
+// Vamos ahora a generar todos estos archivos HTML
 
 import { loadBrawlers } from "./brawler.js";
-import { render } from "./render.js";
+import { render, renderBrawler } from "./render.js";
 import { writeFile } from "fs/promises";
 
-// Cargamos los brawlers
+// Cargamos los brawlers 
 const brawlers = await loadBrawlers();
 
-// Generamos el HTML
+// Generamos primero el index
 const html = render(brawlers);
-
-// Creamos el archivo index.html en nuestro directorio
 await writeFile("index.html", html);
 
+// Generamos ahora el HTML para cada brawler (los guardamos en la carpeta "brawlers" que hemos creado)
+for (const b of brawlers) {
+  const html = renderBrawler(b);
+  await writeFile(`brawlers/${b.name.toLowerCase()}.html`, html);
+}
+
+// Nos hemos dado cuenta que algunas imágenes de los HTML individuales salen más grandes que otras.
+// En la siguiente versión de nuestros HTML intentaremos mejorar esto
--- render.js
+++ render.js
@@ -1,21 +1,57 @@
-// El siguiente paso es visualizar una página sencilla que muestre únicamente la lista de brawlers
+// Ahora que ya hemos conseguido una página sencilla y hemos visto que funciona correctamente, lo siguiente
+// es, además de mantener el index con la lista de los brawlers, crear una página individual para cada uno
+// con los campos de su clase. Haremos también que se pueda acceder a cada página simplemente pinchando en el nombre
+// del index. Es por ello, que necesitaremos dos funciones: una para el index, y otra para crear cada una de las 
+// páginas individuales. Cada uno de los HTML, los guardaremos en una carpeta que vamos a llamar "brawlers"
+
+// Primero, vamos a modificar un poco la función "render" para que al pinchar en los nombres,
+// nos lleve directamente al HTML correspondiente. Nótese que también vamos a poner todos
+// los HTML en minúsculas (esto es "por convenio")
 
 export const render = (brawlers) => {
   return `
 <html>
   <head>
     <meta charset="UTF-8">
-    <title>HTML de Brawl Stars v1</title>
+    <title>HTML de Brawl Stars v2</title>
   </head>
   <body>
-    <h1>Lista de Brawlers</h1>
+    <h1>Brawlers</h1>
     ${brawlers.map(b => `
-      <div>${b.name}</div>
-    `).join("")} 
+      <div>
+        <a href="brawlers/${b.name.toLowerCase()}.html">${b.name}</a>
+      </div>
+    `).join("")}
   </body>
 </html>`;
 };
 
-// Hemos usado .join("") ya que .map devuelve un array y al acceder posteriormente al index,
-// los distintos nombres de los Brawlers aparecerían en líneas separadas pero también con una coma.
-// Al usar el .join("") le decimos a la función una los nombres pero sin ningún separador
+// A continuación, creamos la función que genera los HTML individuales con todos los campos
+// de la clase Brawler (el ID finalmente no lo usaremos, pero nos pareció interesante añadirlo a la clase
+// por si pudiese ser útil en algún momento)
+
+export const renderBrawler = (b) => {
+  return `
+<html>
+  <head>
+    <meta charset="UTF-8">
+    <title>${b.name}</title>
+  </head>
+  <body>
+    <a href="../index.html">RETURN</a>
+    <h1>${b.name}</h1>
+    <img src="${b.imageUrl}" alt="${b.name}" />
+    <p><strong>Rareza:</strong> ${b.rarity}</p>
+    <p><strong>Clase:</strong> ${b.clase}</p>
+    <p>${b.description}</p>
+    <h2>Star Powers</h2>
+    <ul>
+      ${b.starPowers.map(sp => `<li>${sp}</li>`).join("")}
+    </ul>
+    <h2>Gadgets</h2>
+    <ul>
+      ${b.gadgets.map(g => `<li>${g}</li>`).join("")}
+    </ul>
+  </body>
+</html>`;
+};
